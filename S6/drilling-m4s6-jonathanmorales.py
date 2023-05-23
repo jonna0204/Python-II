709e7f9bda9c79ab7cb8a9b8679aaefb3431f316
jonna0204 <130108957+jonna0204@users.noreply.github.com>
2023-05-23 10:34:06 -0400

    Add files via upload

--- S6/drilling-m4s6-jonathanmorales.py
+++ S6/drilling-m4s6-jonathanmorales.py
@@ -1,16 +1,28 @@
+#usuarios = {'001':'Marca','002':'Mónica','003':'Jacob'}
+#id_usuario = '004'
+#añadir = 'La clave 004 no está en el diccionario. Añadiendo clave...'
+#
+#try:
+#    print(usuarios[id_usuario])
+#except KeyError: 
+#    print(añadir)
+#usuarios[id_usuario]= None
+#
+#print(usuarios)
 
-def error(a,b):
-    c = f'La clave {b} no está en el diccionario. Añadiendo clave...'
-    try:
+def error(a,b,c): #se crea funcion error
+    try:    #de aca invocamos try
         print(a[b])
-    except KeyError:
+    except: #se añade el mensaje de error
         print(c)
-    
-    a[b] = None
+    a[b]= None
 
-usuarios = {'001','Marca','002','Monica','003','Jacob'}
+usuarios = {'001':'Marca','002':'Mónica','003':'Jacob'}
 id_usuario = '004'
+mensaje = 'La clave 004 no está en el diccionario. Añadiendo clave...'
 
-error(usuarios,id_usuario)
+
+error(usuarios,id_usuario,mensaje)
 
 print(usuarios)
+
