35ff7fb3d1b250b8382ba7efda52d6695c2ed7eb
jonna0204 <130108957+jonna0204@users.noreply.github.com>
2023-06-09 13:41:23 -0400

    evaluacion

---
+++ EVALUACION/SEGUNDA_PARTE/evaluacion2.py
@@ -0,0 +1,89 @@
+import time
+
+class Vehiculo:
+    def __init__(self, marca, modelo, nruedas):
+        self.marca = marca
+        self.modelo = modelo
+        self.nruedas = nruedas
+
+    def __str__(self):
+        return f'Marca: {self.marca}, Modelo: {self.modelo}, Ruedas: {self.nruedas}'
+
+class Automovil(Vehiculo):
+    def __init__(self, marca, modelo, nruedas, velocidad, cilindrada):
+        super().__init__(marca, modelo, nruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return f'{super().__str__()}, Velocidad: {self.velocidad} Km/h, Cilindrada: {self.cilindrada} cc.'
+
+class Particular(Automovil):
+    def __init__(self, marca, modelo, nruedas, velocidad, cilindrada,numero_puestos):
+        super().__init__(marca, modelo, nruedas, velocidad, cilindrada)
+        self.numero_puestos = numero_puestos
+    
+    def __str__(self):
+        return f'{super().__str__()}, Velocidad: {self.velocidad} Km/h, Cilindrada: {self.cilindrada} cc, Numeros puestos {self.numero_puestos}'
+
+class Carga(Automovil):
+    def __init__(self, marca, modelo, nruedas, velocidad, cilindrada,peso_carga):
+        super().__init__(marca, modelo, nruedas, velocidad, cilindrada)
+        self.peso_carga = peso_carga
+    
+    def __str__(self):
+        return f'{super().__str__()}, Velocidad: {self.velocidad} Km/h, Cilindrada: {self.cilindrada} cc, Peso de carga {self.peso_carga}'
+
+class Bicicleta(Vehiculo):
+    def __init__(self, marca, modelo, nruedas,tipo):
+        super().__init__(marca, modelo, nruedas)
+        self.tipo = tipo
+    
+    def __str__(self):
+        return f'Marca: {self.marca}, Modelo: {self.modelo}, Ruedas: {self.nruedas}, Tipo: {self.tipo}'
+
+class Motocicleta(Bicicleta):
+    def __init__(self, marca, modelo, nruedas, tipo,numero_radios,cuadro,motor):
+        super().__init__(marca, modelo, nruedas, tipo)
+        self.numero_radios = numero_radios
+        self.cuadro = cuadro
+        self.motor = motor
+
+    def __str__(self):
+        return f'Marca: {self.marca}, Modelo: {self.modelo}, Ruedas: {self.nruedas}, Tipo: {self.tipo},Numero de radios: {self.numero_radios}, Cuadro: {self.cuadro}, Motor: {self.motor}'
+
+"""
+def agregar_datos():
+    time.sleep(1)
+    marca = input("Inserte la marca del automóvil: ")
+    modelo = input("Inserte el modelo: ")
+    numero_ruedas = int(input("Inserte el número de ruedas: "))
+    velocidad = int(input("Inserte la velocidad: "))
+    cilindrada = int(input("Inserte la cilindrada: "))
+    print("**********************************************************")
+
+    return Automovil(marca, modelo, numero_ruedas, velocidad, cilindrada)  
+
+def imprimir_datos(lista):
+    for i, automovil in enumerate(lista, 1):
+        print(f"Datos del automóvil {i} : Marca {automovil.marca}, Modelo {automovil.modelo}, {automovil.nruedas} ,ruedas {automovil.velocidad} Km/h, {automovil.cilindrada} cc")
+"""
+
+particular = Particular("Ford", "Fiesta", 4, "180", "500", 5)
+carga = Carga("Daft Trucks", "G 38", 10, 120, "1000", "20000")
+bicicleta = Bicicleta("Shimano", "MT Ranger", 2, "Carrera")
+motocicleta = Motocicleta("BMW", "F800s",2,"Deportiva","2T","Doble Viga", 21)
+
+
+print(particular.__dict__)
+print(carga.__dict__)
+print(bicicleta.__dict__)
+print(motocicleta.__dict__)
+
+print("Motocicleta es instancia con relación a Vehículo:", isinstance(motocicleta, Vehiculo))
+print("Motocicleta es instancia con relación a Automovil:", isinstance(motocicleta, Automovil))
+print("Motocicleta es instancia con relación a Vehículo particular:", isinstance(motocicleta, Particular))
+print("Motocicleta es instancia con relación a Vehículo de Carga:", isinstance(motocicleta, Carga))
+print("Motocicleta es instancia con relación a Bicicleta:", isinstance(motocicleta, Bicicleta))
+print("Motocicleta es instancia con relación a Motocicleta:", isinstance(motocicleta, Motocicleta))
+
